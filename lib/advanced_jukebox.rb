aa4438f2b1ebae2e5fb764f8b365bf5ff6b67164
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 17:00:46 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -13,15 +13,19 @@
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:
+        - help : displays this help message
+        - list : displays a list of songs you can play
+        - play : lets you choose a song to play
+        - exit : exits this program"
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  (my_songs.keys).each {|title| puts title}
 end
 
 
@@ -33,13 +37,33 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+  puts "Please enter a song name:"
+  input = gets.chomp
+
+  if (my_songs.keys).include?(input)
+    system "open #{my_songs[input]}"
+  else
+    puts "Invalid input, please try again"
+  end
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
+def run(array)
+  help
+  input = nil
+  until input == "exit"
+    puts "Please enter a command:"
+    input = gets.chomp
+    if input == "list"
+      list(array)
+    elsif input == "help"
+      help
+    else input == "play"
+      play(array)
+    end
+  end
+  exit_jukebox
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,53 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+        - help : displays this help message
+        - list : displays a list of songs you can play
+        - play : lets you choose a song to play
+        - exit : exits this program"
+end
+
+def list(array)
+  array.each_with_index do |element, i|
+    puts "#{i + 1}. #{element}"
+  end
+end
+
+def play(array)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  songs = {}
+  array.each_with_index do |title, i|
+    songs[i + 1] = title
+  end
+  if songs[input.to_i]
+    puts "Playing " + songs[input.to_i]
+  elsif array.include?(input)
+    puts "Playing " + input
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(array)
+  help
+  input = nil
+  until input == "exit"
+    puts "Please enter a command:"
+    input = gets.chomp
+    if input == "list"
+      list(array)
+    elsif input == "help"
+      help
+    else input == "play"
+      play(array)
+    end
+  end
+  exit_jukebox
+end
