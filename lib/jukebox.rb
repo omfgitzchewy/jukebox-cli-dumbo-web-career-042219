ff86db663c9dc299ea425ccae16c1671c3509596
Aaron Chu <aaronchu357@gmail.com>
2019-04-11 15:10:24 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,8 +28,8 @@ def play(array)
   puts "Please enter a song name or number:"
   input = gets.chomp
 
-  if array.include?(input) == true
-    puts "Playing #{input}"
+  if array.include?(input)
+    puts "Playing" input
   else
     "Invalid input, please try again"
   end
